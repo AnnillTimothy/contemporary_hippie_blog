4e5c3d21fc900ec605a7ef5ed8045823080b34ff
Annill Timothy <annillseale17@gmail.com>
2019-08-13 22:06:34 +0200

    Altered list view and detail view templates. As well as contact page.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class HomePage(TemplateView):
+    template_name = 'blog/home.html'
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def contact_us(request):
+    return render(request, 'blog/contact_us.html', {})
+
+
+'''
+
+def post_category(request, category):
+    posts = Post.objects.filter()
+    return render(request, 'blog/category_list.html', {'category': category}) 
+
+'''
